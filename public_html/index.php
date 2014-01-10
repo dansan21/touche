7512018f13b9f33519a2143e07abf6ec2c532f59
root <root@touche.(none)>
2014-01-10 10:02:27 -1000

    Updated file structure

--- public_html/index.php
+++ public_html/index.php
@@ -1,42 +1,32 @@
 <?
 #
 # Copyright (C) 2002 David Whittington
-# Copyright (C) 2005 Steve Overton
 #
 # See the file "COPYING" for further information about the copyright
 # and warranty status of this work.
 #
-# arch-tag: index.php
+# arch-tag: admin/index.php
 #
-    include_once("lib/config.inc");
-    include_once("lib/data.inc");
+#    include("lib/admin_config.inc");
+    include("lib/create.inc");
 
-session_name("TOUCHE-$db_name");
+session_name("TOUCHE-CREATE");
 session_start();
 $_SESSION = array();
 
-if($_SERVER['REQUEST_METHOD'] == 'GET') {
+if($_SERVER['REQUEST_METHOD'] == 'GET'){
 
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C/DTD HTML 4.0 Transitional//EN">
 <html>
 <head>
-<link rel="shortcut icon" href="images/favicon.ico" />
 <script language="javascript">
     function set_focus() {
 	if (document.f.user.value) {
 	    document.f.password.focus();
 	} else {
-	document.f.user.focus();
+	    document.f.user.focus();
 	}
-    }
-    function check_input() {
-    	if(document.f.user.value.length == 0 || document.f.password.value.length == 0)
-	{
-		alert("Please fill out all fields.");
-		return false;
-	}
-	return true;
     }
 </script>
 </head>
@@ -47,33 +37,33 @@ if($_SERVER['REQUEST_METHOD'] == 'GET') {
 <table cellpadding="1" cellspacing="0" border="0" bgcolor="#000000"><tr><td>
 <table cellpadding="5" cellspacing="0" border="0" bgcolor="<?=$title_bg_color?>"><tr><td>
 <font color="#ffffff">
-<b><?php echo $contest_name ?></b><br>
-<small><?php echo $contest_host ?></small>
+<b>Touche</b><br>
+<small>Creating Contest</small>
 </font>
 </td></tr><tr><td bgcolor="#ffffff">
+<?
 
-<?php
-    if(isset($state) && $state == 1) {
+    if (isset($state) && $state == 1) {
 	echo "<center><font color=#cc0000><b>";
 	echo "Login or Password Invalid</b></font></center>\n";
-    } else if(isset($state) && $state == 2) {
+    }
+    else if (isset($state) && $state == 2) {
 	echo "<center><font color=#cc0000><b>";
-	echo "You are not yet logged in</b></font></center>\n";
+	    echo "You are not yet logged in</b></font></center>\n";
     }
 
-?>
-
-<table cellpadding="5" cellspacing="0" border="0">
-<tr><td>Login:</td><td><input type="text" name="user" id="user" size="20">
+?><table cellpadding="5" cellspacing="0" border="0">
+<tr><td>Login:</td><td><input type="text" name="user" size="20">
 </td></tr>
-<tr><td>Password:</td><td><input type="password" name="password" id="password" size="20"></td></tr>
-<tr><td>&nbsp;</td><td><input type="submit" name="submit" value="  OK  " onclick="return check_input()">
+<tr><td>Password:</td><td><input type="password" name="password" size="20"></td></tr>
+<tr><td>&nbsp;</td><td><input type="submit" name="submit" value="  OK  ">
 <input type="reset" name="submit" value=" Cancel "></td></tr>
 </table>
 </td></tr></table>
 </td></tr></table>
 </td></tr></table>
 </form>
+
 </body>
 </html>
 <?
@@ -81,17 +71,17 @@ if($_SERVER['REQUEST_METHOD'] == 'GET') {
 else if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $user = $_POST['user'];
     $password = $_POST['password'];
-   
-    if($password == $users[$user]['password']) {
-	$_SESSION['contestant_user'] = $user;
-	$_SESSION['contestant_pass'] = $password;
-	$_SESSION['team_id'] = $users[$user]['team_id'];
-	$_SESSION['team_name'] = $users[$user]['team_name'];
-	header ("Location: main.php");
+
+    if($user == $create_user && $password == $create_pass) {
+	$_SESSION['admin_user'] = $user;
+	$_SESSION['admin_pass'] = $password;
+	header ("Location: createcontest.php");
+	exit(0);
     }
     else {
-	header ("Location: index.php?state=1");
+    	header ("Location: index.php?state=1");
+	exit(0);
     }
 }
-
 ?>
+
