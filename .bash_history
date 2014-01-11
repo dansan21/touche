ae803bdcf4988683be4f2bf70df6c6d444379612
root <root@touche.(none)>
2014-01-11 07:36:24 -1000

    working create contest and access admin panel

---
+++ .bash_history
@@ -0,0 +1,257 @@
+ls
+git status
+rm -rf *
+ls
+git status
+git fetch upstream
+sudo git fetch upstream
+sudo git remote add upstream https://github.com/geisler/touche
+sudo fit fetch upstream
+sudo git fetch upstream
+git status
+ls
+git merge upstream/master
+sudo git merge upstream/master
+ls
+rm -rf *
+sudo rm -rf *
+ls
+git pull origin master
+sudo git pull origin master
+ls
+git clone https://github.com/dansan21/touche
+ls
+git status
+git fetch upstream
+sudo git fetch upstream
+ls
+git merge upstream/master
+sudo git merge upstream/master
+ls
+rm -rf .*
+sudo rm -rf .*
+ls
+sudo rm -rf *
+ls
+git init
+git pull origin master
+git git remote add origin https://github.com/dansan21/touche.git
+sudo git remote add origin https://github.com/dansan21/touche.git
+ls
+sudo git pull origin master
+ls
+git remote add upstream https://github.com/geisler/touche.git
+ls
+sudo git fetch upstream
+ls
+git add
+git add .
+ls
+sudo git fetch upstream
+ls
+sudo git pull upstream
+sudo git pull upstream master
+ls
+git merge upstream/master
+ls
+git push origin master
+sudo git push origin master
+ls
+cd ..
+ls
+rm -rf judge
+sudo rm -rf judge
+ls
+cd daniel
+git add .
+ls
+git push origin master
+sudo git push origin master
+ls
+git status
+git add/rm .
+sudo git add/rm *
+git commit
+sudo git commit
+ls
+git commit
+sudo git commit
+git push origin master
+sudo git push origin master
+ls
+git status
+git add/rm
+git add *
+git rm *
+git rm readme
+git rm -r readme
+git rm -r develop
+git status
+git rm master-crontab.crom
+git rm master-crontab.cron
+git rm index.php
+git rm dbcreate.sql
+git rm createcontest.php
+git rm createcontest2.php
+git rm touche*
+git rm Touch*
+git rm READ*
+git rm COPY*
+ls
+git status
+git rm -r *
+sudo git rm -r *
+ls
+git reset
+ls
+git add *
+ls
+git add *
+sudo git reset
+ls
+git status
+git checkout
+ls
+sudo git checkout
+git pull origin master
+ls
+git status
+git add publi*
+ls
+git status
+git rm --cache
+git rm --cached
+git rm --cached *
+git rm -r --cached *
+ls
+git status
+git rm -r --cached .git add .
+sudo git rm -r --cached .git add .
+ls
+git status
+sudo git add/rm *
+git --help
+git rm --cache *
+sudo git rm --cache *
+git status
+git rm --cached *
+git add lib/
+git add public_html
+ls
+git status
+git commit
+sudo git commit
+git status
+git rm develop
+git rm -r develop
+ls
+git status
+git rm -r readme
+ls
+git status
+git remove CO*\
+git remove CO*
+git rm CO*
+git rm RE*
+git rm Tu*
+git rm To*
+git rm crea*
+git rm d*
+git rm in*
+git rm m*
+git status
+sudo git commit
+ls
+git status
+git push origin master
+sudo git push origin master
+ls
+cd ..
+ls -l
+cp -pr ../develop/
+cp -pr ../develop/ 
+ls
+cd daniel
+ls
+git status
+git add develop
+ls
+git status
+git commit added develop folder
+git commit -m "added develop folder"
+sudo git commit -m "added develop folder"
+ls
+git push origin master
+sudo git push origin master
+ls
+ls -l
+chmod 644 public_html
+ls
+ls -l
+ls
+cd public_html
+ls
+cd public_html
+cd public_html
+exit
+cd public_html
+ls
+ls -l
+ls -l
+cd public_html
+ls
+ls -l
+cd ..
+cd .htaccess
+ls
+chmod 777 public_html
+ls
+ls -l
+chmod 777 -r public_html
+chmod -r 777 public_html
+ls
+cd public_html
+ls
+cd ..
+ls- l
+ls -l
+cd public_html
+ls
+ls -l
+ls -a
+test.php
+ls
+cd ..
+ls -l
+chmod o+x public_html
+ls -l
+ls -ld
+chmod o+x ./
+cd public_html
+ls -l
+chmod +x *.php
+chmod g-w *.php
+ls -l
+cd ..
+ls
+cd ..
+ls
+chmod -R 755 daniel
+ls
+cd danile
+cd daniel
+ls
+ls -l
+ls -l
+cd public_html
+ls -l
+cd ..
+ls
+cd ..
+ls
+--enable-force-cgi-redirect
+--enable-pcntl
+ls
+cd daniel
+ls
+git status
--- public_html/dbcreate.sql
+++ public_html/dbcreate.sql
@@ -1,28 +1,3 @@
-# phpMyAdmin MySQL-Dump
-# version 2.3.2
-# http://www.phpmyadmin.net/ (download page)
-#
-# Host: abe
-# Generation Time: Jan 4, 2005 at 09:27 AM
-# Server version: 3.23.54
-# PHP Version: 4.1.1
-# Database : `prog_contest`
-# --------------------------------------------------------
-
-#
-# Copyright (C) 2003 David Whittington
-# Copyright (C) 2005 Steve Overton
-# Copyright (C) 2005 David Crim
-# Copyright (C) 2009 Jonathan Geisler
-#
-# See the file "COPYING" for further information about the copyright
-# and warranty status of this work.
-#
-# arch-tag: dbcreate.sql
-#
-#
-# Table structure for table `CLARIFICATION_REQUESTS`
-#
 DROP TABLE IF EXISTS CLARIFICATION_REQUESTS;
 
 CREATE TABLE CLARIFICATION_REQUESTS (
@@ -30,17 +5,13 @@ CREATE TABLE CLARIFICATION_REQUESTS (
   TEAM_ID int(11) NOT NULL default '0',
   PROBLEM_ID int(11) NOT NULL default '0',
   SUBMIT_TS int(11) NOT NULL default '0',
-  QUESTION text NOT NULL,
+  QUESTION char(100) NOT NULL,
   REPLY_TS int(11) NOT NULL default '0',
-  RESPONSE text NOT NULL,
+  RESPONSE char(100) NOT NULL,
   BROADCAST int(11) NOT NULL default '0',
   PRIMARY KEY  (CLARIFICATION_ID)
-) TYPE=MyISAM;
-# --------------------------------------------------------
+);
 
-#
-# Table structure for table `JUDGED_SUBMISSIONS`
-#
 DROP TABLE IF EXISTS JUDGED_SUBMISSIONS;
 
 CREATE TABLE JUDGED_SUBMISSIONS (
@@ -51,15 +22,11 @@ CREATE TABLE JUDGED_SUBMISSIONS (
   ATTEMPT int(11) NOT NULL default '0',
   SOURCE_FILE char(255) NOT NULL default '',
   RESPONSE_ID int(11) NOT NULL default '0',
-  VIEWED int(11) NOT NULL default '0',
-  JUDGED int(10) NOT NULL default '0',
+  AUTO_RESPONSE_ID int(11) NOT NULL default '0',
+  VIEWED int(10) NOT NULL default '0',
   PRIMARY KEY  (JUDGED_ID)
-) TYPE=MyISAM;
-# --------------------------------------------------------
+);
 
-#
-# Table structure for table `QUEUED_SUBMISSIONS`
-#
 DROP TABLE IF EXISTS QUEUED_SUBMISSIONS;
 
 CREATE TABLE QUEUED_SUBMISSIONS (
@@ -70,12 +37,8 @@ CREATE TABLE QUEUED_SUBMISSIONS (
   ATTEMPT int(11) NOT NULL default '0',
   SOURCE_FILE char(255) NOT NULL default '',
   PRIMARY KEY  (QUEUE_ID)
-) TYPE=MyISAM;
-# --------------------------------------------------------
+);
 
-#
-# Table structure for table `RESPONSES`
-#
 DROP TABLE IF EXISTS RESPONSES;
 
 CREATE TABLE RESPONSES (
@@ -83,26 +46,20 @@ CREATE TABLE RESPONSES (
   RESPONSE char(50) NOT NULL default '',
   RESPONSE_COLOR char(255) NOT NULL default '',
   PRIMARY KEY  (RESPONSE_ID)
-) TYPE=MyISAM;
+);
 
-#
-# Set default table data
-#
+
 INSERT INTO RESPONSES VALUES("0","Pending","FFFF00");
-INSERT INTO RESPONSES VALUES("9","Accepted","00FF00");
+INSERT INTO RESPONSES VALUES("1","Accepted","00FF00");
 INSERT INTO RESPONSES VALUES("2","Forbidden Word in Source","FF0000");
-INSERT INTO RESPONSES VALUES("1","Undefined File Type","FF0000");
-INSERT INTO RESPONSES VALUES("3","Compile Error","FF0000");
-INSERT INTO RESPONSES VALUES("4","Exceeds Time Limit","FF0000");
+INSERT INTO RESPONSES VALUES("3","Undefined File Type","FF0000");
+INSERT INTO RESPONSES VALUES("4","Compile Error","FF0000");
+INSERT INTO RESPONSES VALUES("5","Exceeds Time Limit","FF0000");
 INSERT INTO RESPONSES VALUES("6","Incorrect Output","FF0000");
 INSERT INTO RESPONSES VALUES("7","Format Error","FF0000");
-INSERT INTO RESPONSES VALUES("5","Runtime Error","FF0000");
-INSERT INTO RESPONSES VALUES("8","Error (Reason Unknown)","FF0000");
-# --------------------------------------------------------
+INSERT INTO RESPONSES VALUES("8","Runtime Error","FF0000");
+INSERT INTO RESPONSES VALUES("9","Error (Reason Unknown)","FF0000");
 
-#
-# Table structure for table `PROBLEMS`
-#
 DROP TABLE IF EXISTS PROBLEMS;
 
 CREATE TABLE PROBLEMS (
@@ -111,18 +68,14 @@ CREATE TABLE PROBLEMS (
   PROBLEM_LOC char(20) NOT NULL default '',
   PROBLEM_NOTE char(100) NOT NULL default '',
   PRIMARY KEY  (PROBLEM_ID)
-) TYPE=MyISAM;
-# --------------------------------------------------------
+);
 
-#
-# Table structure for table `TEAMS`
-#
 DROP TABLE IF EXISTS TEAMS;
 
 CREATE TABLE TEAMS (
   TEAM_ID int(11) NOT NULL auto_increment,
-  TEAM_NAME char(100) NOT NULL default '',
-  ORGANIZATION char(100) NOT NULL default '',
+  TEAM_NAME char(30) NOT NULL default '',
+  ORGANIZATION char(50) NOT NULL default '',
   USERNAME char(30) NOT NULL default '',
   PASSWORD char(20) NOT NULL default '',
   SITE_ID int(11) NOT NULL default '0',
@@ -133,51 +86,35 @@ CREATE TABLE TEAMS (
   ALTERNATE_NAME char(30) NOT NULL default '',
   EMAIL char(30) NOT NULL default '',
   PRIMARY KEY  (TEAM_ID)
-) TYPE=MyISAM;
-# --------------------------------------------------------
+);
 
-#
-# Table structure for table `CATEGORY_TEAM`
-#
 DROP TABLE IF EXISTS CATEGORY_TEAM;
 
 CREATE TABLE CATEGORY_TEAM (
   TEAM_ID int(11) NOT NULL default '0',
   CATEGORY_ID int(11) NOT NULL default'0',
   PRIMARY KEY  (TEAM_ID,CATEGORY_ID)
-) TYPE=MyISAM;
-# --------------------------------------------------------
+);
 
-#
-# Table structure for table `CATEGORIES`
-#
 DROP TABLE IF EXISTS CATEGORIES;
 
 CREATE TABLE CATEGORIES (
   CATEGORY_ID int(11) NOT NULL auto_increment,
   CATEGORY_NAME char(30) NOT NULL default '',
   PRIMARY KEY  (CATEGORY_ID)
-) TYPE=MyISAM;
-# --------------------------------------------------------
+);
 
-#
-# Table structure for table `SITE`
-#
 DROP TABLE IF EXISTS SITE;
 
 CREATE TABLE SITE (
   SITE_ID int(11) NOT NULL auto_increment,
   SITE_NAME char(30) NOT NULL default '',
-  START_TIME time NOT NULL default '0',
+  START_TIME time NOT NULL,
   PRIMARY KEY  (SITE_ID),
   START_TS int(11) NOT NULL default '0',
   HAS_STARTED int(11) NOT NULL default '0'
-) TYPE=MyISAM;
-# --------------------------------------------------------
+);
 
-#
-# Table structure for table `CONTEST_CONFIG`
-#
 DROP TABLE IF EXISTS CONTEST_CONFIG;
 
 CREATE TABLE CONTEST_CONFIG (
@@ -198,12 +135,8 @@ CREATE TABLE CONTEST_CONFIG (
   START_TS int(11) NOT NULL default '0',
   HAS_STARTED int(11) NOT NULL default '0',
   TEAM_SHOW smallint(1) NOT NULL default '0'
-) TYPE=MyISAM;
-# --------------------------------------------------------
+);
 
-#
-# Table structure for table `LANGUAGE`
-#
 DROP TABLE IF EXISTS LANGUAGE;
 
 CREATE TABLE LANGUAGE (
@@ -214,73 +147,53 @@ CREATE TABLE LANGUAGE (
   REPLACE_HEADERS int(1) NOT NULL default '0',
   CHECK_BAD_WORDS int(1) NOT NULL default '0',
   PRIMARY KEY  (LANGUAGE_ID)
-) TYPE=MyISAM;
+);
 
-#
-# Set default table data
-#
+
 INSERT INTO LANGUAGE VALUES(NULL,"C","30","c_jail",
 	"1","1");
-INSERT INTO LANGUAGE VALUES(NULL,"CXX","30","cpp_jail",
+INSERT INTO LANGUAGE VALUES(NULL,"C++","30","cpp_jail",
 	"1","1");
 INSERT INTO LANGUAGE VALUES(NULL,"JAVA","60","java_jail",
 	"0","0");
-# --------------------------------------------------------
 
-#
-# Table structure for table `FILE_EXTENSIONS`
-#
 DROP TABLE IF EXISTS FILE_EXTENSIONS;
 CREATE TABLE FILE_EXTENSIONS (
   EXT_ID int(11) NOT NULL auto_increment,
   EXT char(10) NOT NULL default '',
   PRIMARY KEY  (EXT_ID)
-) TYPE=MyISAM;
+);
 
-#
-# Set default table data
-#
+
 INSERT INTO FILE_EXTENSIONS VALUES(NULL,"c");
 INSERT INTO FILE_EXTENSIONS VALUES(NULL,"C");
 INSERT INTO FILE_EXTENSIONS VALUES(NULL,"cpp");
 INSERT INTO FILE_EXTENSIONS VALUES(NULL,"cc");
 INSERT INTO FILE_EXTENSIONS VALUES(NULL,"java");
-# --------------------------------------------------------
 
-#
-# Table structure for table `LANGUAGE_FILE_EXTENSIONS`
-#
 DROP TABLE IF EXISTS LANGUAGE_FILE_EXTENSIONS;
 
 CREATE TABLE LANGUAGE_FILE_EXTENSIONS (
   EXT_ID int(11) NOT NULL default '0',
   LANGUAGE_ID int(11) NOT NULL default '0',
   PRIMARY KEY  (EXT_ID,LANGUAGE_ID)
-) TYPE=MyISAM;
+);
 
-#
-# Set default table data
-#
+
 INSERT INTO LANGUAGE_FILE_EXTENSIONS VALUES(1,1);
 INSERT INTO LANGUAGE_FILE_EXTENSIONS VALUES(2,2);
 INSERT INTO LANGUAGE_FILE_EXTENSIONS VALUES(3,2);
 INSERT INTO LANGUAGE_FILE_EXTENSIONS VALUES(4,2);
 INSERT INTO LANGUAGE_FILE_EXTENSIONS VALUES(5,3);
-# --------------------------------------------------------
 
-#
-# Table structure for table `FORBIDDEN_WORDS`
-#
 DROP TABLE IF EXISTS FORBIDDEN_WORDS;
 
 CREATE TABLE FORBIDDEN_WORDS (
   LANGUAGE_ID int(11) NOT NULL default '0',
   WORD char(15) NOT NULL default ''
-) TYPE=MyISAM;
+);
 
-#
-# Set default table data
-#
+
 INSERT INTO FORBIDDEN_WORDS VALUES(1,"system");
 INSERT INTO FORBIDDEN_WORDS VALUES(1,"fstream");
 INSERT INTO FORBIDDEN_WORDS VALUES(1,"open");
@@ -299,21 +212,15 @@ INSERT INTO FORBIDDEN_WORDS VALUES(2,"connect");
 INSERT INTO FORBIDDEN_WORDS VALUES(2,"accept");
 INSERT INTO FORBIDDEN_WORDS VALUES(2,"listen");
 INSERT INTO FORBIDDEN_WORDS VALUES(2,"mmap");
-# --------------------------------------------------------
 
-#
-# Table structure for table `HEADERS`
-#
 DROP TABLE IF EXISTS HEADERS;
 
 CREATE TABLE HEADERS (
   LANGUAGE_ID int(11) NOT NULL default '0',
   HEADER char(15) NOT NULL default ''
-) TYPE=MyISAM;
+);
 
-#
-# Set default table data
-#
+
 INSERT INTO HEADERS VALUES("1","stdio.h");
 INSERT INTO HEADERS VALUES("1","stdlib.h");
 INSERT INTO HEADERS VALUES("1","string.h");
@@ -349,49 +256,28 @@ INSERT INTO HEADERS VALUES("3","java.lang.*");
 INSERT INTO HEADERS VALUES("3","java.io.*");
 INSERT INTO HEADERS VALUES("3","java.util.*");
 INSERT INTO HEADERS VALUES("3","java.math.*");
-#------------------------------------------------------------
 
-#
-# Table structure for table QUEUED_COMPILE
-#
-#
-#DROP TABLE IF EXISTS QUEUED_COMPILE;
-#
-#CREATE TABLE QUEUED_COMPILE (
-#  COMPILE_ID int(11) NOT NULL auto_increment,
-#  TEAM_ID int(11) NOT NULL default '0',
-#  PROBLEM_ID int(11) NOT NULL default '0',
-#  TS int(11) NOT NULL default '0',
-#  SOURCE_FILE varchar(255) NOT NULL,
-#  PRIMARY KEY  (COMPILE_ID)
-#) TYPE=MyISAM;
-#------------------------------------------------------------
-#
-#
-# Table structure for table JUDGED_COMPILE
-#
-#
-#DROP TABLE IF EXISTS JUDGED_COMPILE;
-#
-#CREATE TABLE JUDGED_COMPILE (
-#  COMPILE_ID int(11) NOT NULL auto_increment,
-#  TEAM_ID int(11) NOT NULL default '0',
-#  TS int(11) NOT NULL default '0',
-#  SOURCE_FILE varchar(255) NOT NULL,
-#  RESPONSE_ID int(11) NOT NULL default '0',
-#  AUTO_RESPONSE_ID int(11) NOT NULL default '0',
-#  PRIMARY KEY  (COMPILE_ID)
-#) TYPE=MyISAM;
 
-#
-# Table structure for table AUTO_RESPONSES
-#
+DROP TABLE IF EXISTS QUEUED_COMPILE;
 
-DROP TABLE IF EXISTS AUTO_RESPONSES;
+CREATE TABLE QUEUED_COMPILE (
+  COMPILE_ID int(11) NOT NULL auto_increment,
+  TEAM_ID int(11) NOT NULL default '0',
+  PROBLEM_ID int(11) NOT NULL default '0',
+  TS int(11) NOT NULL default '0',
+  SOURCE_FILE varchar(255) NOT NULL,
+  PRIMARY KEY  (COMPILE_ID)
+);
 
-CREATE TABLE AUTO_RESPONSES (
-  JUDGED_ID int(11) NOT NULL default '0',
-  IN_FILE varchar(255) NOT NULL, 
-  AUTO_RESPONSE int(10) NOT NULL default '0',
-  ERROR_NO int(10) default NULL
-) TYPE=MyISAM;
+
+DROP TABLE IF EXISTS JUDGED_COMPILE;
+
+CREATE TABLE JUDGED_COMPILE (
+  COMPILE_ID int(11) NOT NULL auto_increment,
+  TEAM_ID int(11) NOT NULL default '0',
+  TS int(11) NOT NULL default '0',
+  SOURCE_FILE varchar(255) NOT NULL,
+  RESPONSE_ID int(11) NOT NULL default '0',
+  AUTO_RESPONSE_ID int(11) NOT NULL default '0',
+  PRIMARY KEY  (COMPILE_ID)
+);
