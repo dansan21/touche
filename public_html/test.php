2aad555d709830c63fb59169824a20cb75bacb09
Daniel <daniel_sanders@taylor.edu>
2014-01-15 09:07:37 -1000

    Moved start contest over to admin. Added c and c++ jails. Bug Fixes.

--- public_html/test.php
+++ public_html/test.php
@@ -1,51 +1,27 @@
 <?php
+
 include_once("lib/session.inc");
 include_once("lib/create.inc");
-if($_POST['B1'] == "Submit") {
-   $contest = $_POST['contest_name'];
-   $dbhost = $_POST['dbhost'];
-   $dbpw = $_POST['dbpassword'];
-   $HOST = $_POST['contest_host'];
-}
-?>
-<html>
-<body bgcolor="<?=$page_bg_color?>" link="0000cc" alink="000066" vlink="0000cc">
-<table width="90%" align="center" cellpadding="1" cellspacing="0" border="0" bgcolor="#000000">
-        <tr><td>
-                <table width="100%" cellpadding="5" cellspacing="0" border="0">
-                        <tr bgcolor="<?=$title_bg_color?>">
-                                <td>
-                                <!-- Beautification hack. 2006-09-25 -sb -->
 
-                                <font color="#ffffff">
-                                <b>Creating Contest</b>  <small></small>
-                                </font>
-                                </td>
-                                <td align="right">
-                                         <font color="#ffffff">
-                                         <b>ADMIN</b>
-                                         </font>
-                                </td>
-                        </tr>
-                        <tr>
-                                <td bgcolor="#ffffff" colspan="2">
-				<center><b>
-<?php
+//change this information
+$contest = "danieltest";
+   $dbhost = "localhost";
+   $dbpw = "password";
+   $HOST = "Daniel";
 
 $user = `whoami`;
 $user = str_replace("\n","",$user);
 $user = str_replace(" ","",$user);
 
-$dbPass = $dbpw;
 $dbU = "contest_skeleton";
 $db_name = preg_replace("/ /", "_", $contest);
 $contest_noesc = $contest;
 $contest = preg_replace("/ /", "\ ", $contest);
 $contest_dir = "../$contest";
-$base_dir = "/home/$user/$contest";
 echo "<p>Creating contest folder (takes a while) . . . \n";
    $cmd = "cp -pr ../develop/ ";
    $cmd .= $contest_dir;
+   echo $cmd;
    system($cmd, $result);
 echo "Finished.</p>\n";
 echo "<p>Clearing folders . . . ";
@@ -53,106 +29,71 @@ echo "<p>Clearing folders . . . ";
    $cmd2 .= $contest_dir;
    $cmd2 .= "/data/*";
    system($cmd2, $result);
-   echo "Result: $result \n";
    $cmd2 = "rm -rf ";
    $cmd2 .= $contest_dir;
    $cmd2 .= "/judged/*";
    system($cmd2, $result);
-   echo "Result: $result \n";
    $cmd2 = "rm -rf ";
    $cmd2 .= $contest_dir;
    $cmd2 .= "/queue/*";
    system($cmd2, $result);
-   echo "Result: $result \n";
    $cmd2 = "rm -rf ";
    $cmd2 .= $contest_dir;
    $cmd2 .= "/test_compile/*";
    system($cmd2, $result);
-   echo "Result: $result \n";
    $cmd2 = "rm -rf ";
    $cmd2 .= $contest_dir;
-   $cmd2 .= "/c_jail/home/$user/*";
+   $cmd2 .= "/c_jail/home/$contest/*";
    system($cmd2, $result);
-   echo "Result: $result \n";
    $cmd2 = "rm -rf ";
    $cmd2 .= $contest_dir;
-   $cmd2 .= "/cpp_jail/home/$user/*";
+   $cmd2 .= "/cpp_jail/home/$contest/*";
    system($cmd2, $result);
-   echo "Result: $result \n";
    $cmd2 = "rm -rf ";
    $cmd2 .= $contest_dir;
-   $cmd2 .= "/java_jail/home/$user/*";
+   $cmd2 .= "/java_jail/home/$contest/*";
    system($cmd2, $result);
-   echo "Result: $result \n";
 echo"Finished.</p>\n";
 echo "<p>Making Directories . . . ";
    $cmd2 = "mkdir -p ";
    $cmd2 .= $contest_dir;
-   $cmd2 .= "/cpp_jail/home";
-   system($cmd2, $result);
-   echo "Result: $result \n";
-   $cmd2 = "mkdir -p ";
-   $cmd2 .= $contest_dir;
-   $cmd2 .= "/c_jail/home/$user";
-   system($cmd2, $result);
-   echo "Result: $result \n";
-   $cmd2 = "mkdir -p ";
-   $cmd2 .= $contest_dir;
-   $cmd2 .= "/c_jail/home/$user/";
+   $cmd2 .= "/c_jail/home/$contest/";
    $cmd2 .= $contest;
    $cmd2 .= "/judged";
    system($cmd2, $result);
-   echo "Result: $result \n";
    $cmd2 = "mkdir -p ";
    $cmd2 .= $contest_dir;
-   $cmd2 .= "/c_jail/home/$user/";
+   $cmd2 .= "/c_jail/home/$contest/";
    $cmd2 .= $contest;
    $cmd2 .= "/data";
    system($cmd2, $result);
-   echo "Result: $result \n";
    $cmd2 = "mkdir -p ";
    $cmd2 .= $contest_dir;
-   $cmd2 .= "/cpp_jail/home";
-   system($cmd2, $result);
-   echo "Result: $result \n";
-   $cmd2 = "mkdir -p ";
-   $cmd2 .= $contest_dir;
-   $cmd2 .= "/cpp_jail/home/$user";
-   system($cmd2, $result);
-   echo "Result: $result \n";
-   $cmd2 = "mkdir -p ";
-   $cmd2 .= $contest_dir;
-   $cmd2 .= "/cpp_jail/home/$user/";
+   $cmd2 .= "/cpp_jail/home/$contest/";
    $cmd2 .= $contest;
    $cmd2 .= "/judged";
    system($cmd2, $result);
-   echo "Result: $result \n";
    $cmd2 = "mkdir -p ";
    $cmd2 .= $contest_dir;
-   $cmd2 .= "/cpp_jail/home/$user/";
+   $cmd2 .= "/cpp_jail/home/$contest/";
    $cmd2 .= $contest;
    $cmd2 .= "/data";
    system($cmd2, $result);
-   echo "Result: $result \n";
    $cmd2 = "mkdir -p ";
    $cmd2 .= $contest_dir;
-   $cmd2 .= "/java_jail/home/$user/";
+   $cmd2 .= "/java_jail/home/$contest/";
    $cmd2 .= $contest;
    $cmd2 .= "/judged";
    system($cmd2, $result);
-   echo "Result: $result \n";
    $cmd2 = "mkdir -p ";
    $cmd2 .= $contest_dir;
-   $cmd2 .= "/java_jail/home/$user/";
+   $cmd2 .= "/java_jail/home/$contest/";
    $cmd2 .= $contest;
    $cmd2 .= "/data";
    system($cmd2, $result);
-   echo "Result: $result \n";
 echo"Finished.</p>\n";
-
 echo "<p> Adding in jail files. . . ";
 //cpp_jail
-	$user = str_replace("\n","",$user);
 	$cmd = "cp -R /home/$user/jails/cpp_jail/lib64 /home/$user/$contest/cpp_jail/lib64";
 	system($cmd,$result);
 	echo "Result: $result \n";
@@ -165,7 +106,9 @@ echo "<p> Adding in jail files. . . ";
 	$cmd = "cp -R /home/$user/jails/cpp_jail/bin /home/$user/$contest/cpp_jail/bin";
 	system($cmd,$result);
 	echo "Result: $result \n";
-
+	$cmd = "cp -R /home/$user/jails/cpp_jail/home/$user /home/$user/$contest/cpp_jail/home/$user";
+	system($cmd,$result);
+	echo "Result: $result \n";
 //c_jail	
 	$cmd = "cp -R /home/$user/jails/c_jail/lib64 /home/$user/$contest/c_jail/lib64";
 	system($cmd,$result);
@@ -179,20 +122,21 @@ echo "<p> Adding in jail files. . . ";
 	$cmd = "cp -R /home/$user/jails/c_jail/bin /home/$user/$contest/c_jail/bin";
 	system($cmd,$result);
 	echo "Result: $result \n";
+	$cmd = "cp -R /home/$user/jails/c_jail/home/$user /home/$user/$contest/c_jail/home/$user";
+	system($cmd,$result);
+	echo "Result: $result \n";
 	
 	$test = "chmod -R 0755 /home/$user/$contest";
 	system($test);
-	echo "Finished</p>";
-	
+
 echo "<p>Creating Database . . . ";
-   $cmd3 = "mysqladmin --password=$dbPass -u root create $db_name";
+   $cmd3 = "mysqladmin --password=$dbpw -u root create $db_name";
    system($cmd3, $result);
-   $cmd3 = "mysql --password=$dbPass -u root $db_name < dbcreate.sql";
+   $cmd3 = "mysql --password=$dbpw -u root $db_name < dbcreate.sql";
    system($cmd3, $result);
    $cmd4 = "cp -r develop/ ./";
    $cmd4 .= $contest;
    system($cmd4, $result);
-   
 $link = mysql_connect($dbhost, $dbU, $dbpw);
 if (!$link) {
     print "Sorry.  Database connect failed.";
@@ -205,7 +149,9 @@ if (!$connect_good) {
     exit;
 }
 
-$contest_info = mysql_query("INSERT INTO CONTEST_CONFIG (HOST, CONTEST_NAME, FREEZE_DELAY, CONTEST_END_DELAY, BASE_DIRECTORY, JUDGE_USER) VALUES ('$HOST', '$contest', '14400', '18000', '$base_dir', 'judge')");
+$base_dir = "/home/$user/" . $contest;
+echo $base_dir;
+$contest_info = mysql_query("INSERT INTO CONTEST_CONFIG (HOST, CONTEST_NAME, FREEZE_DELAY, CONTEST_END_DELAY, BASE_DIRECTORY, JUDGE_USER, JUDGE_PASS) VALUES ('$HOST', '$contest', '14400', '18000', '$base_dir', 'judge', 'judge')");
 if (!$contest_info) {
     print "Sorry.  Database request (INSERT) failed.";
     exit;
@@ -215,6 +161,7 @@ echo"Finished.</p>";
 
 #-----------editing database.inc----------------------------------
 echo "<p>Editing Settings . . . ";
+echo "$contest_noesc/lib/database.inc";
 $fhdl = fopen("$contest_noesc/lib/database.inc", "r") OR die("Error with opening file 1");
 $file = fread($fhdl, filesize("$contest_noesc/lib/database.inc"));
 $file = preg_replace("/YOUR.DB.HOST/", $dbhost, $file);
@@ -225,22 +172,23 @@ $fhdl = fopen("$contest_noesc/lib/database.inc", "w") OR die("Error with opening
 $chk = fwrite($fhdl, $file);
 fclose($fhdl);
 
-
-#-------------------Copy new database.inc for admin-------------------------
+#-----------Copying over new database.inc to admin----------------
 $cmd = "cp $contest_noesc/lib/database.inc $contest_noesc/admin/lib/database.inc";
 system($cmd,$result);
 
-
-#-------------------Copy new database.inc for judge-------------------------
+#-----------Copying over new database.inc to judge----------------
 $cmd = "cp $contest_noesc/lib/database.inc $contest_noesc/judge/lib/database.inc";
 system($cmd,$result);
 
 #---------editing chroot_wrapper.c--------------------------------
-$fhdl = fopen("../$contest_noesc/chroot_wrapper.c", "r") OR die("Error with opening file");
+echo "<br/> trying to open ../$contest_noesc/chroot_wrapper.c<br/>\n";
+$fhdl = fopen("../$contest_noesc/chroot_wrapper.c", "r") OR die("Error with opening file 4");
 $file = fread($fhdl, filesize("../$contest_noesc/chroot_wrapper.c"));
 $file = preg_replace("/develop/", "$contest", $file);
+#$file = preg_replace("/502/", "success", $file);
+#$file = preg_replace("/need if diff/", "502", $file);
 fclose($fhdl);
-$fhdl = fopen("../$contest_noesc/chroot_wrapper.c", "w") OR die("Error with opening file");
+$fhdl = fopen("../$contest_noesc/chroot_wrapper.c", "w") OR die("Error with opening file 3");
 $chk = fwrite($fhdl, $file);
 fclose($fhdl);
 $cmd5 = "gcc -o ../$contest_noesc/chroot_wrapper.exe ../$contest_noesc/chroot_wrapper.c";
@@ -276,7 +224,7 @@ $chk = fwrite($fhdl, $file);
 fclose($fhdl);
 
 #-----------------------------------------------------------------
-echo "<p>To finish setting up the contest go to: <a href='http://touche.cse.taylor.edu/~$user/$contest_noesc/admin/index.php'>Administration setup</a></p>";
+echo "<p>To finish setting up the contest go to: <a href='http://touche.cse.taylor.edu/~$user/$contest_noesc/admin/test.php'>Administration setup</a></p>";
 ?>
 </center></b></td></tr>
 </body>
